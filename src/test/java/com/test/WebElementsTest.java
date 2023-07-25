1020476af4206339b3910010b1a546e4e9186c86
Antonio Trindade <atrindade@umov.me>
2023-07-25 20:07:14 -0300

    Adicionado teste textfield

--- src/test/java/com/test/WebElementsTest.java
+++ src/test/java/com/test/WebElementsTest.java
@@ -5,32 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebElementsTest {
-	
+
 	private WebDriver driver;
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "/home/atrindade/Dev/drivers/chromedriver");	
-		//System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/home/atrindade/Dev/drivers/chromedriver");
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://antoniotrindade.com.br/treinoautomacao/elementsweb.html");			
+		driver.get("http://antoniotrindade.com.br/treinoautomacao/elementsweb.html");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		
-	
+		driver.quit();
 	}
 
 	@Test
-	public void testValidaTextField() {
-		
-		
-		
-	}
+	public void testValidaTextField() throws InterruptedException {
 
+		// Identifica o elemento na página
+		WebElement textFieldBox1 = driver.findElement(By.name("txtbox1"));
+
+		// Faz a iteração com o elemento
+		textFieldBox1.sendKeys("Antônio Trindade");
+
+		Thread.sleep(3000);
+
+		// faz a validação
+		assertEquals("Antônio Trindade", textFieldBox1.getAttribute("value"));
+	}
 }
