b4a1b37fa10d63001a19bb39b8a272257e96f054
Antonio Trindade <atrindade@umov.me>
2023-07-25 20:50:57 -0300

    Cria teste rb

--- src/test/java/com/test/WebElementsTest.java
+++ src/test/java/com/test/WebElementsTest.java
@@ -2,6 +2,8 @@ package com.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -39,17 +41,43 @@ public class WebElementsTest {
 
 		Thread.sleep(3000);
 
-		// faz a validação
+		// Faz a validação
 		assertEquals("Antônio Trindade", textFieldBox1.getAttribute("value"));
+	}
+
+	@Test
+	public void testValidaStatusTextFields() {
+		// Identificar os elementos
+		WebElement textFieldBoxEnable = driver.findElement(By.name("txtbox1"));
+		WebElement textFieldBoxDisable = driver.findElement(By.name("txtbox2"));
+
+		// Faz a validação
+		assertTrue(textFieldBoxEnable.isEnabled());
+		assertFalse(textFieldBoxDisable.isEnabled());
 	}
 	
 	@Test
-	public void testValidaStatusTextFields() {
-		// Indentificar os elementos
-		WebElement textFieldBoxEnable = driver.findElement(By.name("txtbox1"));
-		WebElement textFieldBoxDisable = driver.findElement(By.name("txtbox2"));
+	public void testValidaRadioButton() throws InterruptedException {
+		// Identicar os elementos
+		List<WebElement> radioButtons = driver.findElements(By.name("radioGroup1"));
 		
-		assertTrue(textFieldBoxEnable.isEnabled());
-		assertFalse(textFieldBoxDisable.isEnabled());		
+		//radioButtons.get(2).click();
+		
+		for (WebElement rb : radioButtons) {
+			//System.out.println(rb.getAttribute("value"));
+			if (rb.getAttribute("value").equals("Radio 3")) {
+				rb.click();
+				break;
+			}
+		}
+				
+		// valida tamanho da lista
+		assertEquals(4, radioButtons.size());
+		
+		assertTrue(radioButtons.get(2).isSelected());
+		
+		assertFalse(radioButtons.get(0).isSelected());
+		assertFalse(radioButtons.get(1).isSelected());
+		assertFalse(radioButtons.get(3).isSelected());		
 	}
 }
