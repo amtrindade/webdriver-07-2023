accf21bb817bf4aa65c5f4a8aa6d4a3ab4216c19
Antonio Trindade <atrindade@umov.me>
2023-08-01 21:31:17 -0300

    Adiciona xpath

---
+++ src/test/java/com/test/TableTest.java
@@ -0,0 +1,57 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import java.time.Duration;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TableTest {
+	
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/home/atrindade/Dev/drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("http://antoniotrindade.com.br/treinoautomacao/localizandovalorestable.html");
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+	
+	@Test
+	public void testCopiaNome() throws InterruptedException {
+		String nome = "Fulano";
+		
+		WebElement td = driver.findElement(By.xpath("//td[contains(text(),'"+nome+"')]"));
+		String nomeCapturado = td.getText();
+		
+		WebElement tfReserva = driver.findElement(By.xpath("//*[@id='txt01']"));
+		tfReserva.sendKeys(nomeCapturado);
+		
+		Thread.sleep(3000);		
+		assertEquals("Fulano da Silva", tfReserva.getAttribute("value"));
+		
+	}
+	
+	@Test
+	public void testCheckBox() throws InterruptedException {
+		WebElement cb = driver.findElement(By.xpath("//*[.='Fulano da Silva']/../td/input"));
+		cb.click();
+		
+		assertTrue(cb.isSelected());
+		Thread.sleep(3000);
+	}
+
+}
